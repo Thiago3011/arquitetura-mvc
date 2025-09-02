04aa6d32ec5da484d0f0c8906d023cd0f11e4533
Thiago3011 <thiago.silva1001@outlook.com>
2025-09-02 20:23:25 -0300

    feat: implementing project routes for pets and people

---
+++ src/main/composer/pet_delete_composer.py
@@ -0,0 +1,11 @@
+from src.models.sqlite.settings.connection import db_connection_handler
+from src.models.sqlite.repositories.pets_repository import PetsRepository
+from src.controllers.pet_delete_controller import PetdeleteController
+from src.views.pet_delete_view import PetDeleteView
+
+def pet_delete_composer():
+    model = PetsRepository(db_connection_handler)
+    controller = PetdeleteController(model)
+    view = PetDeleteView(controller)
+
+    return view
--- src/main/routes/pets_routes.py
+++ src/main/routes/pets_routes.py
@@ -1,7 +1,26 @@
 from flask import Blueprint, jsonify
+from src.views.http_types.http_request import HttpRequest
+
+from src.main.composer.pet_list_composer import pet_list_composer
+from src.main.composer.pet_delete_composer import pet_delete_composer
 
 pet_route_bp = Blueprint("pets_routes", __name__)
 
 @pet_route_bp.route("/pets", methods=["GET"])
 def list_pets():
-    return jsonify({"pets": "Cachorro"}), 200
+    http_request = HttpRequest()
+
+    view = pet_list_composer()
+    http_response = view.handle(http_request)
+
+    return jsonify(http_response.body), http_response.status_code
+
+@pet_route_bp.route('/pets/<name>', methods=['DELETE'])
+def delete_pet(name):
+
+    http_request = HttpRequest(param={ "name": name })
+    view = pet_delete_composer()
+
+    http_response = view.handle(http_request)
+
+    return jsonify(http_response.body), http_response.status_code
